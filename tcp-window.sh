bed7179ac1e037cba4c88ae32d22fb6c1235d62f
ForestL <45709305+ForestL18@users.noreply.github.com>
2025-08-20 00:00:55 +0800

    Update tcp-window.sh

--- tcp-window.sh
+++ tcp-window.sh
@@ -31,22 +31,31 @@ cp /etc/sysctl.conf /etc/sysctl.conf.bak.$(date +%F-%T)
 
 cat >/etc/sysctl.conf<<EOF
 fs.file-max = 524288
-net.ipv4.tcp_congestion_control = bbr
+
+fs.file-max = 524288
 net.core.default_qdisc = fq
-net.ipv4.tcp_slow_start_after_idle = 0
-#net.ipv4.tcp_mtu_probing = 1
-net.ipv4.tcp_rmem = 8192 262144 536870912
-net.ipv4.tcp_wmem = 4096 16384 536870912
-#net.ipv4.udp_rmem_min = 8192
-#net.ipv4.udp_wmem_min = 8192
+net.core.somaxconn = 4096
+net.ipv4.tcp_max_syn_backlog = 4096
+net.ipv4.tcp_congestion_control = bbr
+net.core.rmem_max = 67108848
+net.core.wmem_max = 67108848
+#net.ipv4.tcp_rmem = 8192 262144 536870912
+#net.ipv4.tcp_wmem = 4096 16384 536870912
+net.ipv4.tcp_rmem = 8192 262144 16777216
+net.ipv4.tcp_wmem = 4096 16384 16777216
 net.ipv4.tcp_adv_win_scale = -2
+net.ipv4.tcp_slow_start_after_idle = 0
 net.ipv4.tcp_notsent_lowat = 131072
+net.ipv4.tcp_sack = 1
+net.ipv4.tcp_timestamps = 1
+kernel.panic = -1
+vm.swappiness = 0
+
 #net.ipv6.conf.all.disable_ipv6 = 1
 #net.ipv6.conf.default.disable_ipv6 = 1
 #net.ipv6.conf.lo.disable_ipv6 = 1
-#net.ipv4.ip_forward = 1
 EOF
 
 rm tcp-window.sh
 
-sleep 3 && reboot >/dev/null 2>&1
+#sleep 3 && reboot >/dev/null 2>&1
